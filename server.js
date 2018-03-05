b7ede95f364675ed793abfe8bdafec47e1edfbe8
Alexa Scott <arscott4792@gmail.com>
2018-03-05 13:25:28 -0800

    completed drills

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 'use strict';
 
+const express = require('express');
+
 // TEMP: Simple In-Memory Database
-const data = require('./db/notes');
+const data = require('./db/notes.json');
 
-console.log('hello world!');
 
-// INSERT EXPRESS APP CODE HERE...
+const app = express();
+app.use(express.static('public'));
+
+app.get('/api/notes/', (req, res) => {
+  //http://localhost:8080/api/notes/?searchTerm=cats
+  const searchTerm = req.query.searchTerm;
+  const filteredNote = data.filter(note => note.title.includes(searchTerm)) ;
+  res.json(filteredNote);
+});
+
+const findById = (id) => {
+  return data.find(note => note.id === id);
+};
+
+app.get('/api/notes/:id', (req, res) => {
+  // console.log(req.params.id);
+  res.json(findById(req.params.id));
+});
+
+//Listen for incoming connections
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
+
+
